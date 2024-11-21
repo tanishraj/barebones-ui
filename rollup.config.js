2c33b2547127e623d7ee346ccd034902ea0a3bf5
Tanish Raj <learn.tanishraj@gmail.com>
2024-11-21 07:22:48 +0400

    feat: rollup setup and npm package setup

--- rollup.config.js
+++ rollup.config.js
@@ -4,39 +4,67 @@ import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import typescript from 'rollup-plugin-typescript2';
 import postcss from 'rollup-plugin-postcss';
-import pkg from './package.json' with { type: 'json' };
+import { readFileSync } from 'fs';
+import typescriptEngine from 'typescript';
+import terser from '@rollup/plugin-terser';
+
+const packageJson = JSON.parse(readFileSync('./package.json'));
 
 export default {
-  input: 'src/index.ts',
+  input: './src/index.ts',
   output: [
     {
-      file: pkg.main, // CommonJS format
-      format: 'cjs',
-      sourcemap: true,
-    },
-    {
-      file: pkg.module, // ES Module format
+      // file: packageJson.module,
+      // format: 'es',
+      // exports: 'named',
+      // sourcemap: false,
+      dir: 'dist',
       format: 'es',
       sourcemap: true,
+      preserveModules: true,
     },
   ],
   plugins: [
-    peerDepsExternal(), // Automatically externalize peerDependencies
+    peerDepsExternal(),
     resolve({
-      extensions: ['.js', '.jsx', '.ts', '.tsx'], // Resolve these extensions
-    }), // Locate and bundle dependencies in node_modules
-    commonjs(), // Convert CommonJS modules to ES6
-    json(), // Allow Rollup to import JSON files
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    }),
+    commonjs(),
+    json(),
     typescript({
-      tsconfig: './tsconfig.app.json', // Specify the tsconfig path
-      clean: true, // Clean cache before each build
-    }), // Compile TypeScript
+      tsconfig: './tsconfig.app.json',
+      typescript: typescriptEngine,
+      sourceMap: false,
+      exclude: [
+        'coverage',
+        '.storybook',
+        'storybook-static',
+        'config',
+        'dist',
+        'node_modules/**',
+        '*.cjs',
+        '*.mjs',
+        '**/__snapshots__/*',
+        '**/__tests__',
+        '**/*.test.js+(|x)',
+        '**/*.test.ts+(|x)',
+        '**/*.mdx',
+        '**/*.story.ts+(|x)',
+        '**/*.story.js+(|x)',
+        '**/*.stories.ts+(|x)',
+        '**/*.stories.js+(|x)',
+        'setupTests.ts',
+        'vite.config.ts',
+        'vitest.config.ts',
+      ],
+    }),
     postcss({
-      extract: true, // Extract CSS to a separate file
-      minimize: true, // Minify CSS
-      sourceMap: true, // Enable source maps
-      modules: false, // Disable CSS modules if not needed
-    }), // Process CSS with PostCSS
+      extract: true,
+      minimize: true,
+      sourceMap: true,
+      modules: false,
+    }),
+    terser(),
   ],
-  external: ['react', 'react-dom'], // Mark peerDependencies as external
+  external: ['react', 'react-dom'],
 };
