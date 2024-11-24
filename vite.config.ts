b0fd516d0796604f232aa14f5e7221c31fb062bb
Tanish Raj <learn.tanishraj@gmail.com>
2024-11-25 00:06:50 +0400

    feat: vite build setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,31 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, './src/index.ts'),
+      name: 'backbones-ui',
+      formats: ['es', 'cjs'],
+      fileName: 'backbones-ui',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+  plugins: [react(), dts({ rollupTypes: true })],
   resolve: {
     alias: {
-      '@': '/src',
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
