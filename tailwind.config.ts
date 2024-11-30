15b959472c49e99a28b7e133557e8d4ed8565705
Tanish Raj <learn.tanishraj@gmail.com>
2024-11-30 04:03:20 +0400

    feat: color scheme update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,14 @@
 import type { Config } from 'tailwindcss';
 
-import { colors } from './src/styles/tokens';
+import { colors, semanticColors } from './src/styles/tokens';
 // tailwind.config.js
 export default {
+  darkMode: 'class',
   content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
   theme: {
     colors: {
       ...colors,
+      ...semanticColors,
     },
     extend: {},
   },
