d9ffe4b1244621e842eb318dcf5d8b173604a9a7
Tanish Raj <learn.tanishraj@gmail.com>
2025-02-15 05:52:46 +0400

    fix: removing theme provider

--- src/playground/App.tsx
+++ src/playground/App.tsx
@@ -1,11 +1,8 @@
-import ThemeToggle from './ThemeToggle';
-
 import { Playground } from '@/playground';
 
 export const App = () => {
   return (
     <div className='App flex flex-col gap-5 p-4'>
-      <ThemeToggle />
       <Playground />
     </div>
   );
--- src/playground/main.tsx
+++ src/playground/main.tsx
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom/client';
 
 import '../tailwind.css';
 import { App } from './App';
-import { ThemeProvider } from './ThemeProvider';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
 );
