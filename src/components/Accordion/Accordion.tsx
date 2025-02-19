83b7a6121bb63a4d623c4aff7ee15ad02d02857f
Tanish Raj <learn.tanishraj@gmail.com>
2025-02-19 07:15:14 +0400

    feat: accordion component refactoring

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -2,48 +2,64 @@ import { useState } from 'react';
 import { type VariantProps } from 'class-variance-authority';
 import clsx from 'clsx';
 
-import { accordionContainerStyles, accordionStyles } from './Accordion.styles';
+import {
+  accordionContainerStyles,
+  accordionStyles,
+  titleStyles,
+} from './Accordion.styles';
 import { AccordionItem } from './types';
 
 export interface AccordionProps
   extends VariantProps<typeof accordionStyles>,
+    VariantProps<typeof titleStyles>,
     React.HTMLAttributes<HTMLDivElement> {
+  name: string;
   items: AccordionItem[];
   defaultOpenIndex?: number;
+  expandAll?: boolean;
+  merged?: boolean;
 }
 
 export const Accordion = ({
+  name,
   items,
   bordered,
   iconStyle,
-  className,
   defaultOpenIndex,
+  expandAll,
+  size,
+  merged,
 }: AccordionProps) => {
   const [openIndex, setOpenIndex] = useState<number | null>(
     defaultOpenIndex ?? null,
   );
 
-  const containerClassName = clsx(accordionContainerStyles, className);
+  const containerClassName = accordionContainerStyles({ merged });
   const accordionClassName = accordionStyles({
     bordered,
     iconStyle,
-    className,
+    merged,
   });
 
   return (
     <div className={containerClassName}>
       {items.map((item, index) => (
-        <div key={index} className={accordionClassName}>
+        <div className={accordionClassName}>
           <input
             type='radio'
-            name='accordion'
-            checked={openIndex === index}
+            name={expandAll ? '' : name}
+            defaultChecked={expandAll || index === openIndex}
             onChange={() => setOpenIndex(index)}
           />
-          <div className='collapse-title text-xl font-medium'>{item.title}</div>
-          <div className='collapse-content'>
-            <div>{item.content}</div>
+          <div
+            className={clsx(
+              'collapse-title text-lg font-bold',
+              titleStyles({ size }),
+            )}
+          >
+            {item.title}
           </div>
+          <div className='collapse-content'>{item.content}</div>
         </div>
       ))}
     </div>
