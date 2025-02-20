ee870edcc3fe2264926aa1b2623a51935832a1f4
Tanish Raj <learn.tanishraj@gmail.com>
2025-02-21 00:24:43 +0400

    feat: types and export file

---
+++ src/components/Avatar/types.ts
@@ -0,0 +1,15 @@
+import { VariantProps } from 'class-variance-authority';
+
+import { sizeAndShapeStyles, statusStyles, textStyles } from './Avatar.styles';
+
+export type AvatarSize = VariantProps<typeof sizeAndShapeStyles>['size'];
+export type AvatarStatus = VariantProps<typeof statusStyles>['status'];
+
+export type AvatarStyleProps = VariantProps<typeof sizeAndShapeStyles> &
+  VariantProps<typeof textStyles> &
+  VariantProps<typeof statusStyles>;
+
+export type AvatarItem = {
+  url: string;
+  text: string;
+};
