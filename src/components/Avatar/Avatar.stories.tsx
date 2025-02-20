44aeda59975960d56cb4c3d854ec7c9380d724ec
Tanish Raj <learn.tanishraj@gmail.com>
2025-02-21 00:24:21 +0400

    feat: Avatar Storybook

---
+++ src/components/Avatar/Avatar.stories.tsx
@@ -0,0 +1,37 @@
+import { Meta, StoryObj } from '@storybook/react/*';
+
+import { Avatar } from './Avatar';
+
+const meta: Meta<typeof Avatar> = {
+  title: 'Components/Avatar',
+  component: Avatar,
+  tags: ['autodocs'],
+  argTypes: {
+    url: { control: 'text' },
+    text: { control: 'text' },
+    size: { control: 'select', options: ['xs', 'sm', 'md', 'lg'] },
+    shape: {
+      control: 'select',
+      options: ['squircle', 'hexagon', 'triangle', 'circle', 'square'],
+    },
+    status: {
+      control: 'select',
+      options: ['online', 'offline', 'none'],
+    },
+  },
+  args: {
+    url: 'https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp',
+    text: 'D',
+    size: 'md',
+    shape: 'circle',
+    status: 'none',
+  },
+};
+
+type Story = StoryObj<typeof Avatar>;
+
+export const Default: Story = {
+  render: args => <Avatar {...args} />,
+};
+
+export default meta;
