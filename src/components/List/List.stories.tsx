3bd7411f0949a7e93a351e0a74c8b681cc95195f
Tanish Raj <learn.tanishraj@gmail.com>
2025-03-07 07:33:03 +0400

    feat: component intial setup

--- src/components/List/List.stories.tsx
+++ src/components/List/List.stories.tsx
@@ -1,18 +1,58 @@
-import { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react';
 
 import { List } from './List';
 
-const meta: Meta = {
+const meta: Meta<typeof List> = {
   title: 'Components/List',
   component: List,
   tags: ['autodocs'],
-  argTypes: {},
-  args: {},
+  argTypes: {
+    className: {
+      control: 'text',
+      description: 'Additional class names for the list container',
+    },
+  },
 };
 
 export default meta;
-type Story = StoryObj<typeof List>;
+type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  render: args => <List {...args} />,
+  render: args => (
+    <List {...args}>
+      <List.Row>
+        <List.Col>
+          <img
+            className='size-10 rounded-box'
+            src='https://img.daisyui.com/images/profile/demo/1@94.webp'
+            alt='Album cover'
+          />
+        </List.Col>
+
+        <List.Col>
+          <div className='font-medium'>Dio Lupa</div>
+          <div className='text-xs font-semibold uppercase opacity-60'>
+            Remaining Reason
+          </div>
+        </List.Col>
+      </List.Row>
+
+      <List.Row>
+        <List.Col>
+          <img
+            className='size-10 rounded-box'
+            src='https://img.daisyui.com/images/profile/demo/4@94.webp'
+            alt='Album cover'
+          />
+        </List.Col>
+
+        <List.Col>
+          <div className='font-medium'>Ellie Beilish</div>
+          <div className='text-xs font-semibold uppercase opacity-60'>
+            Bears of a fever
+          </div>
+        </List.Col>
+      </List.Row>
+    </List>
+  ),
 };
--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,14 +1,69 @@
-import { FC, ReactNode } from 'react';
+import { ComponentPropsWithoutRef, forwardRef } from 'react';
+import { cva, type VariantProps } from 'class-variance-authority';
+import clsx from 'clsx';
 
-interface ListProps {
-  children: ReactNode;
-}
+// List component
+const listStyles = clsx('list rounded-box bg-base-100 shadow-md');
 
-export const List: FC<ListProps> = ({ children }) => {
-  return (
-    <div>
-      <h1>List Component</h1>
-      <p>{children}</p>
+type ListProps = ComponentPropsWithoutRef<'ul'>;
+
+const ListComponent = forwardRef<HTMLUListElement, ListProps>(
+  ({ className, children, ...props }, ref) => (
+    <ul ref={ref} className={clsx(listStyles, className)} {...props}>
+      {children}
+    </ul>
+  ),
+);
+
+// ListRow component
+const listRowStyles = 'list-row';
+
+type ListRowProps = ComponentPropsWithoutRef<'li'>;
+
+const ListRowComponent = forwardRef<HTMLLIElement, ListRowProps>(
+  ({ className, children, ...props }, ref) => (
+    <li ref={ref} className={clsx(listRowStyles, className)} {...props}>
+      {children}
+    </li>
+  ),
+);
+
+// ListCol component
+const listColVariants = cva([''], {
+  variants: {
+    wrap: {
+      true: 'list-col-wrap',
+    },
+    grow: {
+      true: 'list-col-grow',
+    },
+  },
+});
+
+type ListColProps = VariantProps<typeof listColVariants> &
+  ComponentPropsWithoutRef<'div'>;
+
+const ListColComponent = forwardRef<HTMLDivElement, ListColProps>(
+  ({ wrap, grow, className, children, ...props }, ref) => (
+    <div
+      ref={ref}
+      className={clsx(listColVariants({ wrap, grow }), className)}
+      {...props}
+    >
+      {children}
     </div>
-  );
-};
+  ),
+);
+
+ListComponent.displayName = 'List';
+ListRowComponent.displayName = 'ListRow';
+ListColComponent.displayName = 'ListCol';
+
+export const ListRow = ListRowComponent;
+export const ListCol = ListColComponent;
+
+// Create final exports with compound components
+export const List = Object.assign(ListComponent, {
+  Row: ListRowComponent,
+  Col: ListColComponent,
+});
