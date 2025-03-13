f7daa682aac87eb216e0922f413a0d340e299432
Tanish Raj <learn.tanishraj@gmail.com>
2025-03-13 04:01:25 +0400

    fix: removed daisyui options from tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,5 @@
 import type { Config } from 'tailwindcss';
-import daisyui from 'daisyui';
 
 export default {
   content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
-  darkMode: ['selector', '[data-theme="dark"]'],
-  daisyui: {
-    styled: true,
-    themes: true,
-    base: true,
-    utils: true,
-    logs: true,
-    rtl: false,
-    prefix: '',
-    darkTheme: 'dark',
-  },
-  plugins: [daisyui],
 } satisfies Config;
