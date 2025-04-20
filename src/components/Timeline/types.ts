e1f350b986824b7ca70c3c8b0e808457b8692613
Tanish Raj <learn.tanishraj@gmail.com>
2025-04-20 14:36:35 +0400

    fix: types update

--- src/components/Timeline/types.ts
+++ src/components/Timeline/types.ts
@@ -1,4 +1,24 @@
+import { VariantProps } from 'class-variance-authority';
+
+import {
+  timelineBorderStyles,
+  timelineContentStyles,
+  timelineStyles,
+} from './Timeline.styles';
+
 export interface TimelineItem {
   startContent?: string;
   endContent?: string;
 }
+
+export type TimelineDirection = VariantProps<
+  typeof timelineStyles
+>['direction'];
+
+export type TimelineLayout = VariantProps<
+  typeof timelineContentStyles
+>['contentLayout'];
+
+export type TimelineVariant = VariantProps<
+  typeof timelineBorderStyles
+>['variant'];
