7b317cc693c0cc7d5e8d5224ca4adc3a6d14c157
Tanish Raj <learn.tanishraj@gmail.com>
2025-04-28 00:40:48 +0400

    fix: breadcumb icons updated

--- src/components/Breadcrumbs/Breadcrumbs.stories.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.stories.tsx
@@ -1,55 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import {
+  Home02 as HomeIcon,
+  Folder as FolderIcon,
+  FilePlus03 as AddDocumentIcon,
+} from '@untitled-ui/icons-react';
 
 import Breadcrumbs, { BreadcrumbsItem } from './Breadcrumbs';
-
-const HomeIcon = ({ className }: { className?: string }) => (
-  <svg
-    xmlns='http://www.w3.org/2000/svg'
-    fill='none'
-    viewBox='0 0 24 24'
-    className={className}
-    role='img'
-  >
-    <path
-      strokeLinecap='round'
-      strokeLinejoin='round'
-      strokeWidth='2'
-      d='M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z'
-    />
-  </svg>
-);
-
-const FolderIcon = ({ className }: { className?: string }) => (
-  <svg
-    xmlns='http://www.w3.org/2000/svg'
-    fill='none'
-    viewBox='0 0 24 24'
-    className={className}
-  >
-    <path
-      strokeLinecap='round'
-      strokeLinejoin='round'
-      strokeWidth='2'
-      d='M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z'
-    />
-  </svg>
-);
-
-const AddDocumentIcon = ({ className }: { className?: string }) => (
-  <svg
-    xmlns='http://www.w3.org/2000/svg'
-    fill='none'
-    viewBox='0 0 24 24'
-    className={className}
-  >
-    <path
-      strokeLinecap='round'
-      strokeLinejoin='round'
-      strokeWidth='2'
-      d='M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z'
-    />
-  </svg>
-);
 
 const meta: Meta<typeof Breadcrumbs> = {
   title: 'Components/Breadcrumb',
