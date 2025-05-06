261daaed18d045d174149e329601be255a9dc41f
Tanish Raj <learn.tanishraj@gmail.com>
2025-05-07 00:03:28 +0400

    feat: radio component implementation

---
+++ src/components/Radio/Radio.tsx
@@ -0,0 +1,28 @@
+import { type VariantProps } from 'class-variance-authority';
+import { ComponentPropsWithoutRef, forwardRef } from 'react';
+
+import { radioStyles } from './Radio.styles';
+import { cn } from '../../utils';
+
+export interface RadioProps
+  extends Omit<ComponentPropsWithoutRef<'input'>, 'size' | 'disabled'>,
+    VariantProps<typeof radioStyles> {}
+
+export const Radio = forwardRef<HTMLInputElement, RadioProps>(
+  ({ className, size, variant, disabled, ...props }, ref) => {
+    const radioClassName = cn(
+      radioStyles({ size, variant, disabled }),
+      className,
+    );
+
+    return (
+      <input
+        type='radio'
+        className={radioClassName}
+        disabled={disabled!}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
