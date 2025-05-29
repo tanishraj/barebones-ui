35c39f2039668437e48350d6dea310a2917db668
Tanish Raj <learn.tanishraj@gmail.com>
2025-05-29 19:40:09 +0400

    feat: setting up the playground

--- src/playground/index.tsx
+++ src/playground/index.tsx
@@ -1,12 +1,14 @@
 import { ButtonExample } from './components/ButtonExample';
 import { DropdownExample } from './components/DropdownExample';
 import { ModalExample } from './components/ModalExample/ModalExample';
+import { RichTextEditorExample } from './components/RichEditorExample';
 import { SwapExample } from './components/SwapExample';
 import { ThemeControllerExample } from './components/ThemeControllerExample';
 
 export const Playground = () => {
   return (
     <div className='flex flex-col space-y-4'>
+      <RichTextEditorExample />
       <ButtonExample />
       <DropdownExample />
       <ModalExample />
