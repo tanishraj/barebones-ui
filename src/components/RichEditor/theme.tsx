125c3f714d3c4d6805d3aa80cdafe0655958323c
Tanish Raj <learn.tanishraj@gmail.com>
2025-06-06 23:43:38 +0400

    feat: todo

--- src/components/RichEditor/theme.tsx
+++ src/components/RichEditor/theme.tsx
@@ -19,6 +19,7 @@ export const theme: EditorThemeClasses = {
     h5: 'text-lg font-medium mt-1 mb-1',
     h6: 'text-base font-medium mt-1 mb-1',
   },
+  // List theme to support multiple level list items
   list: {
     ul: 'list-disc pl-6 mb-2',
     ol: 'list-decimal pl-6 mb-2',
