8899eac7c2f33eaf27bed0280b98dae98abc8b9f
Tanish Raj <learn.tanishraj@gmail.com>
2025-07-23 14:12:28 +0400

    feat: simplify ToolbarPlugin by removing unused dropdown and formatting logic

--- src/components/LexicalFocused/plugins/ToolbarPlugin/ToolbarPlugin.tsx
+++ src/components/LexicalFocused/plugins/ToolbarPlugin/ToolbarPlugin.tsx
@@ -1,11 +1,6 @@
 import { Dispatch, FC, SetStateAction } from 'react';
 import { LexicalEditor } from 'lexical';
 
-import { DropDown, DropDownItem } from '../../components/Dropdown';
-import { useToolbarState } from '../../context/ToolbarContext';
-import { clearFormatting, dropDownActiveClass, formatParagraph } from './utils';
-import { SHORTCUTS } from '../ShortcutsPlugin';
-
 import './ToolbarPlugin.css';
 
 interface ToolbarPluginProps {
@@ -16,49 +11,6 @@ interface ToolbarPluginProps {
   isEditable: boolean;
 }
 
-export const ToolbarPlugin: FC<ToolbarPluginProps> = ({
-  editor,
-  isEditable,
-}) => {
-  const {
-    toolbarState: { blockType },
-  } = useToolbarState();
-
-  return (
-    <div className='toolbar'>
-      <DropDown
-        disabled={!isEditable}
-        buttonClassName='toolbar-item spaced'
-        buttonLabel=''
-        buttonAriaLabel='Formatting options for additional text styles'
-        buttonIconClassName='icon dropdown-more'
-      >
-        <DropDownItem
-          className={
-            'item wide ' + dropDownActiveClass(blockType === 'paragraph')
-          }
-          onClick={() => formatParagraph(editor)}
-        >
-          <div className='icon-text-container'>
-            <i className='icon paragraph' />
-            <span className='text'>Normal</span>
-          </div>
-          <span className='shortcut'>{SHORTCUTS.NORMAL}</span>
-        </DropDownItem>
-
-        <DropDownItem
-          onClick={() => clearFormatting(editor)}
-          className='item wide'
-          title='Clear text formatting'
-          aria-label='Clear all text formatting'
-        >
-          <div className='icon-text-container'>
-            <i className='icon clear' />
-            <span className='text'>Clear Formatting</span>
-          </div>
-          <span className='shortcut'>{SHORTCUTS.CLEAR_FORMATTING}</span>
-        </DropDownItem>
-      </DropDown>
-    </div>
-  );
+export const ToolbarPlugin: FC<ToolbarPluginProps> = () => {
+  return <div className='toolbar'>Toolbar plugin</div>;
 };
